c16bbd64a73e7274551988a402dcb6393378091b
Bassel159 <bxbassel@gmail.com>
2025-05-07 21:31:15 +0300

    Initial commit

--- lib/companyProfile/adminHome.dart
+++ lib/companyProfile/adminHome.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class adminHome extends StatefulWidget {
   const adminHome({super.key});
@@ -9,10 +11,86 @@ class adminHome extends StatefulWidget {
 }
 
 class _adminHomeState extends State<adminHome> {
+  bool isLoading = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("ADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMINADMIN"),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(255, 72, 144, 180),
+        foregroundColor: Colors.white,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text("Home"),
+        /*automaticallyImplyLeading: false,*/
+        actions: [
+          IconButton(
+            onPressed: ()  {
+
+            },
+            icon: Icon(Icons.notifications),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.red),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CircleAvatar(radius: 30, child: Icon(Icons.person, size: 40)),
+                  SizedBox(height: 10),
+                  Text(
+                    "Admin",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed("home");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+              onTap: () {
+                Navigator.of(context).pushNamed("settings");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Logout"),
+              onTap: () async {
+                GoogleSignIn googleSignIn = GoogleSignIn();
+                googleSignIn.disconnect();
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushNamedAndRemoveUntil("login", (route) => false);
+              },
+            ),
+          ],
+        ),
+      ),
+      body:
+      isLoading == true
+          ? Center(child: CircularProgressIndicator())
+          : Container(child: Text("Cont")),
     );
   }
 }
